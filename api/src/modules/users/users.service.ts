5fbddf6717340236493b63f7066ad275f9c34286
João Carser <48493500+JoaoCarser@users.noreply.github.com>
2025-02-05 14:19:38 +0000

    fix: :adhesive_bandage: fix prettier file

--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -6,69 +6,69 @@ import { UsersRepository } from 'src/shared/database/repositories/users.reposito
 
 @Injectable()
 export class UsersService {
-	constructor(private readonly usersRepo: UsersRepository) {}
+  constructor(private readonly usersRepo: UsersRepository) {}
 
-	async create(createUserDto: CreateUserDto) {
-		const { name, email, password } = createUserDto;
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
 
-		const emailExist = await this.usersRepo.findByEmail({
-			where: { email },
-			select: { id: true }
-		});
+    const emailExist = await this.usersRepo.findByEmail({
+      where: { email },
+      select: { id: true },
+    });
 
-		if (emailExist) {
-			throw new ConflictException('This email already in use');
-		}
+    if (emailExist) {
+      throw new ConflictException('This email already in use');
+    }
 
-		const hashedPassword = await hash(password, 12);
+    const hashedPassword = await hash(password, 12);
 
-		const user = await this.usersRepo.create({
-			data: {
-				name,
-				email,
-				password: hashedPassword,
-				categories: {
-					createMany: {
-						data: [
-							// Income
-							{ name: 'Salário', icon: 'salary', type: 'INCOME' },
-							{ name: 'Freelance', icon: 'freelance', type: 'INCOME' },
-							{ name: 'Outro', icon: 'other', type: 'INCOME' },
-							// Expense
-							{ name: 'Casa', icon: 'home', type: 'EXPENSE' },
-							{ name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
-							{ name: 'Educação', icon: 'education', type: 'EXPENSE' },
-							{ name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
-							{ name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
-							{ name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
-							{ name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
-							{ name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
-							{ name: 'Outro', icon: 'other', type: 'EXPENSE' }
-						]
-					}
-				}
-			}
-		});
+    const user = await this.usersRepo.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ],
+          },
+        },
+      },
+    });
 
-		return {
-			name: user.name,
-			email: user.email
-		}
-	}
+    return {
+      name: user.name,
+      email: user.email,
+    };
+  }
 
-	findAll() {
-		return `This action returns all users`;
-	}
+  findAll() {
+    return `This action returns all users`;
+  }
 
-	findOne(id: number) {
-		return `This action returns a #${id} user`;
-	}
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
 
-	update(id: number, updateUserDto: UpdateUserDto) {
-		return `This action updates a #${id} user`;
-	}
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
 
-	remove(id: number) {
-		return `This action removes a #${id} user`;
-	}
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
 }
